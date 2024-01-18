a5b38f48a7389f619ebae2108590c3b3243afc74
Souvik <souviksen147@gmail.com>
2024-01-18 20:52:37 +0530

    multer fuctionality added

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,13 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 
-
 const profileRoutes = require('./routes/profiles')
 const userRoutes = require('./routes/user')
-
+const multer = require('multer')
 
 //express app
 const app = express()
+
 
 //middleware
 app.use(express.json())
@@ -19,9 +19,32 @@ app.use((req,res,next)=>{
   next()
 })
 
+
+
+// file upload or image upload using multer fuction
+
+const fileStorageEngine = multer.diskStorage({
+  destination: function(req, file, cb) {
+    return cb(null, "./largefiles")
+  },
+  filename: function (req, file, cb) {
+    return cb(null, Date.now()+ '__' + file.originalname);
+  }
+})
+
+
+const upload = multer({storage: fileStorageEngine})
+
 //routes
  app.use('/api/profiles',profileRoutes)
  app.use('/api/user', userRoutes)
+
+
+ app.post('/single', upload.single('image'), (req, res) => {
+  console.log(req.file, req.body)
+  res.send("Single file upload success");
+})
+
 
 //connected to db
 mongoose.connect(process.env.MONGO_URI)
