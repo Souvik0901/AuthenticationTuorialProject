ce7c26b8175d2bfe36a4a5122f0867796b594bae
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2024-02-24 17:35:19 +0530

    Update uploadImageToS3.js

--- backend/controllers/uploadImageToS3.js
+++ backend/controllers/uploadImageToS3.js
@@ -2,48 +2,44 @@ const AWS = require('aws-sdk');
 const { v4 } = require('uuid');
 const ProfileImage = require('../models/profileImageModel');
 
-// Initialize S3
+
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_BUCKET_REGION,
 });
 
 const uploadImageToS3 = async (req, res, next) => {
   try {
     const file = req.file;
-    
     const fileName = v4() + '_' + file.originalname;
-
     const params = {
-      Bucket: process.env.AWS_BUCKET_NAME,
+      Bucket: process.env.AWS_BUCKET_NAME,  
       Key: fileName,
       Body: file.buffer, 
       ContentType: file.mimetype
     };
-
     const uploadResult = await s3.upload(params).promise();
+  
 
-    // Create a new profile image document in MongoDB
     const newProfileImage = new ProfileImage({
       userimage: uploadResult.Location
     });
-
-    // Save the profile image document to the database
     const savedProfileImage = await newProfileImage.save();
-
 
     res.json({ 
       imageUrl: uploadResult.Location,
       imageId: savedProfileImage._id
     });
   } catch (err) {
-    console.error('Error uploading image to S3 and saving to DB:', err);
     res.status(500).json({ error: 'Failed to upload image' });
   }
 };
 
 
 
+
 module.exports = {
   uploadImageToS3,
 };
+
